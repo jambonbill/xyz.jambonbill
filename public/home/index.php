274e523d5afc026f9c03bba907e3c0b2d26c09ff
orange <jambonbill@gmail.com>
2022-02-23 21:16:03 +0100

    index

--- public/home/index.php
+++ public/home/index.php
@@ -1,30 +1,70 @@
-<!doctype html>
+<?php
+//show as list
+header('Content-Type: text/html; charset=utf-8');
+session_start();
+
+require __DIR__."/../../vendor/autoload.php";
+
+$B=new Djang\Base;
+$HEN=new JAM\LogHicetnunc($B);
+?>
 <html lang="en">
 <head>
-	<base href="/">
+
 	<meta charset="utf-8" />
 
 	<link rel="icon" type="image/png" href="/favicon.ico" />
-	<link rel="manifest" href="/manifest.json" />
 
-	<meta property="og:title" content="hic et nunc" />
+	<meta property="og:title" content="jambonbill.xyz" />
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://hicetnunc.art" />
-	<meta property="og:image" content="/HEN_avatar.png" />
+	<meta property="og:url" content="https://jambonbill.xyz" />
+	<meta property="og:image" content="img/jambonbill.png" />
 	<meta property="og:description" content="decentralized digital assets" />
 
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:creator" content="@hicetnunc2000" />
-	<meta name="twitter:title" content="hic et nunc" />
+	<meta name="twitter:creator" content="@jambonbill" />
+	<meta name="twitter:title" content="jambonbill" />
 	<meta name="twitter:description" content="decentralized digital assets" />
-	<meta name="twitter:image" content="/HEN_avatar.png" />
-
+	<meta name="twitter:image" content="img/jambonbill.png" />
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 
-	<script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
-	<title>hic et nunc</title>
-	<link href="/static/css/2.fe38e6c6.chunk.css" rel="stylesheet"><link href="/static/css/main.c6e900fc.chunk.css" rel="stylesheet"></head>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/psp-lib.js" type="text/javascript"></script>
+	<title>jambonbill.xyz</title>
+<style type="text/css">
+body{
+	background-color: #111111;
+    font-family: monospace;
+}
+iframe{
+	position:absolute;
+	top:0;
+	left:0;
+}
+</style>
+</head>
 <body>
-	<div id="root"></div>
+
+<div class="container-fluid">
+	<h1 style="color:#dedede">jambonbill.xyz
+		<a href="https://twitter.com/jambonbill" target=twitter><i class="fab fa-twitter"></i></a>
+	</h1>
+	<p style="color:#dedede">
+	<a href="https://hicetnunc.art/jambonbill">https://hicetnunc.art/jambonbill</a> - PETSCII / Textmode
+	</p>
+	<hr />
+
+	<iframe src="https://ipfs.io/ipfs/QmbGJwg7ePqAjzvbFPgNb24kaPwLLeXtKBbywQCgfyLvhs" width=512 height=512 frameborder=0></iframe>
+
+
+<div class="row">
+<?php
+require "grid.php";
+?>
+</div><!-- /row -->
+</div><!-- /container -->
+
 </body>
-</html>
+
